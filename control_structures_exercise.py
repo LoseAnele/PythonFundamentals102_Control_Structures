e72e6ccc89d0d0c8dd331fdcabc7e6e941b3a8dd
Anele Lose <s218200285@mandela.ac.za>
2024-09-15 21:04:49 +0200

    modified and used functions for unit tests

--- control_structures_exercise.py
+++ control_structures_exercise.py
@@ -9,13 +9,14 @@ user_age_str = input("Enter your age: ")
 
 # Convert the user_age_str to an integer
 user_age_int = int(user_age_str)
-# 2. Use conditional statements to categorise the user into one of the following categories:
-if user_age_int < 18: # - If age is less than 18 print "You are a minor."
-    print("You are a minor.") 
-elif user_age_int > 17 and user_age_int < 66: # - If age is between 18 and 65 (inclusive), print "You are an adult."
-    print("You are an adult.")
-else: # - If age is 66 or higher, print "You are a senior citizen."
-    print("You are a senior citizen.")
+def categorize_age(user_age):
+    # 2. Use conditional statements to categorise the user into one of the following categories:
+    if user_age < 18: # - If age is less than 18 print "You are a minor."
+        print("You are a minor.") 
+    elif user_age > 17 and user_age < 66: # - If age is between 18 and 65 (inclusive), print "You are an adult."
+        print("You are an adult.")
+    else: # - If age is 66 or higher, print "You are a senior citizen."
+        print("You are a senior citizen.")
 
 
 """ 
@@ -24,15 +25,15 @@ In this exercise, you will use conditional statements to check if a year is a le
 """
 
 year = 0000
-
-if year % 400 == 0:
-    print(f"{True}")
-elif year % 100 == 0:
-    print(f"{False}")
-elif year % 4 == 0:
-    print(f"{True}")
-else:
-    print(f"{False}")
+def is_leap_year(year_):
+    if year_ % 400 == 0:
+        print(f"{True}")
+    elif year_ % 100 == 0:
+        print(f"{False}")
+    elif year_ % 4 == 0:
+        print(f"{True}")
+    else:
+        print(f"{False}")
 
 
 """
@@ -45,15 +46,17 @@ limit_str = input("Enter a number as the limit: ")
 limit = limit_str  # Convert the limit_str to an integer
 
 # 2. Use a  for loop to iterate from 1 to the user-defined limit (inclusive) and print each number.
-for i in range(1, limit + 1):
-    print(i)
+def print_numbers_for_loop(limit_):
+    for i in range(1, limit_ + 1):
+        print(i)
 
 # Use a while loop to iterate from 1 to the user-defined limit (inclusive) and print each number.
 # Initialise a variable to start the loop
-number = 1
-while number <= 1:
-    print(i) #print number
-    number += 1 # Increment number in each iteration   
+def print_numbers_while_loop(limit_):
+    number = 1
+    while number <= limit_:
+        print(number) #print number
+        number += 1 # Increment number in each iteration   
 
 
 """
@@ -69,9 +72,10 @@ limit = int(limit_str) # Convert the limit_str to an integer
 # 3. Inside the loop, use a loop control statement to skip even numbers and print odd numbers.
 # Fill in the code to achieve the goal:
 
-for number in range(1, limit + 1):
-    if number % 2 != 0: # Add code to check if number is odd and print odd numbers
-        print(number)
+def print_odd_numbers(limit_):
+    for number in range(1, limit_ + 1):
+        if number % 2 != 0: # Add code to check if number is odd and print odd numbers
+            print(number)
 
 
 """
@@ -80,13 +84,14 @@ In this exercise, you will use nested loops to generate a multiplication table.
 """
 # 1. Ask the user for a number as the multiplier and convert it to an integer.
 multiplier_str = input("Enter a number as the multiplier: ")
-# Convert the multiplier_str to an integer
+multi = int(multiplier_str) # Convert the multiplier_str to an integer
 
 # 2. Use nested loops to generate a multiplication table.
 # The outer loop iterates from 10 to 1.
 # The inner loop iterates the user-defined multiplier to 1.
 # Fill in the code to complete the nested loops:
 
-for number_x in range(1, 11):
-    for number_y in range(1):
-        print(f"{number_x} x {number_y} = {number_x * number_y}") # Add code to print the multiplication table
+def multiplication_table(multi_):
+    for number_x in range(1, 11):
+        for number_y in range(1, multi + 1):
+            print(f"{number_x} x {multi_} = {number_x * multi_}") # Add code to print the multiplication table
